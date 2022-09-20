9ab5883dc935bf9ac7a70699c91fc66987a9c196
samirjabib <samir.jc123@gmail.com>
2022-09-20 18:14:20 -0500

    add login page with navbar & signup component

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,8 +1,14 @@
+import { FaFolderMinus } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
-        <div >
-            <h1>header</h1>
+        <div className="absolute top-0 bg-amber-400 w-screen h-20 flex justify-center space-between">
+            <h1 className="text-white font-black bg-red-500 flex items-center text-2xl">Tempura</h1>
+            <Link to='/login'>Login</Link>
+          
+            <FaFolderMinus />
         </div>
     );
 };
--- client/src/pages/sign-up/sign-up.route.jsx
+++ client/src/pages/sign-up/sign-up.route.jsx
@@ -3,19 +3,24 @@ import Header from "../../components/header/header.component";
 
 const SignUp = () => {
     return (
-        <div>
+        <div className="relative">
             <BackgroundImage/>
             <Header/>
-            <div></div>
-            <h1>Unlimited Animes</h1>
-            <h4>Watch anywhere. Cancel anytime</h4>
-            <h6>Ready to watch? Enter your email to create or restart membership</h6>
-            <form>
-                <input type="email" placeholder="Email adress" name="email"/>
-                <input type="password" placeholder="Password" name="password"/>
-                <input type="password" placeholder="Confirm password" name="confirm password"/>
-                <button> Get Started</button>
-            </form>
+            <div className="absolute top-0 left-0 bg-black/20 h-screen w-screen flex flex-col justify-center items-center ">
+    
+                <div className="bg-white">
+                    <h1>Unlimited Animes</h1>
+                    <h4>Watch anywhere. Cancel anytime</h4>
+                    <h6>Ready to watch? Enter your email to create or restart membership</h6>
+                    <form>
+                        <input type="text" placeholder='Display Name' name='name'/>
+                        <input type="email" placeholder="Email adress" name="email"/>
+                        <input type="password" placeholder="Password" name="password"/>
+                        <input type="password" placeholder="Confirm password" name="confirm password"/>
+                        <button> Get Started</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
