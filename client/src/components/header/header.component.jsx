4382ccd40723c35eb2c55df1ac80bc946873197d
samirjabib <samir.jc123@gmail.com>
2022-09-20 21:38:32 -0500

    make a navbar responsive

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,14 +1,48 @@
-import { FaFolderMinus } from 'react-icons/fa'
-import { Link } from 'react-router-dom';
-
+import { GiHamburgerMenu } from 'react-icons/gi'
+import Logo from '../../assets/logo-tempura-anime.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className="absolute top-0 bg-amber-400 w-screen h-20 flex justify-center space-between">
-            <h1 className="text-white font-black bg-red-500 flex items-center text-2xl">Tempura</h1>
-            <Link to='/login'>Login</Link>
+        <div className="absolute top-0 bg-amber-700 w-screen h-20 flex justify-between items-center p-4 text-white z-50">
           
-            <FaFolderMinus />
+            <Link to="/" className="cursor-pointer hover:text-yellow-300 duration-500" onClick={() => console.log("hice click")}>
+                <img 
+                    src={Logo} 
+                    alt='logo'
+                    className='w-12 p-2'
+                    />
+                <h1 className="font-bold text-sm relative -top-2">Tempura</h1>  
+            </Link>
+        
+            
+            <ul className=' md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-black/70 w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500'>
+                <li className='mx-2'>
+                    <Link to='#' className='text-xl hover:text-yellow-300 duration-500'>
+                        Sign Up
+                    </Link>
+                </li>
+                <li className='mx-2'>
+                    <Link to='#' className='text-xl hover:text-yellow-300 duration-500'>
+                        Login
+                    </Link>
+                </li>
+               
+                <li className='mx-2'>
+                    <Link to='#' className='text-xl hover:text-yellow-300 duration-500'>
+                        Animes
+                    </Link>
+                </li>
+               
+                <li className='mx-2'>
+                    <Link to='#' className='text-xl hover:text-yellow-300 duration-500'>
+                        My Anime List
+                    </Link>
+                </li>
+               
+            
+            </ul>
+            <GiHamburgerMenu className='relative top-2 hidden' size={30} />
         </div>
     );
 };
--- client/src/pages/sign-up/sign-up.route.jsx
+++ client/src/pages/sign-up/sign-up.route.jsx
@@ -7,18 +7,17 @@ const SignUp = () => {
             <BackgroundImage/>
             <Header/>
             <div className="absolute top-0 left-0 bg-black/20 h-screen w-screen flex flex-col justify-center items-center ">
-    
-                <div className="bg-white">
-                    <h1>Unlimited Animes</h1>
-                    <h4>Watch anywhere. Cancel anytime</h4>
-                    <h6>Ready to watch? Enter your email to create or restart membership</h6>
-                    <form>
-                        <input type="text" placeholder='Display Name' name='name'/>
-                        <input type="email" placeholder="Email adress" name="email"/>
-                        <input type="password" placeholder="Password" name="password"/>
-                        <input type="password" placeholder="Confirm password" name="confirm password"/>
-                        <button> Get Started</button>
-                    </form>
+                <div className="max-w-[300px] h-[500px] bg-black/75 text-white text-center relative top-10">
+                    <div className="max-w-[320px] mx-auto py-16 " >
+                        <h2 className="text-3xl font-bold">Sign In</h2>
+                        <form>
+                            <input type="text" placeholder='Display Name' name='name'/>
+                            <input type="email" placeholder="Email adress" name="email"/>
+                            <input type="password" placeholder="Password" name="password"/>
+                            <input type="password" placeholder="Confirm password" name="confirm password"/>
+                            <button> Get Started</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
